8624209b357d4214463aa27b2c24ef02da37d48c
qwerty2586 <qwerty2@students.zcu.cz>
2017-05-02 00:47:42 +0200

    switch to gradle dep

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,9 @@ import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 
 /**
  * Created by qwerty on 1. 5. 2017.
@@ -11,8 +14,15 @@ class Main : Application() {
 
 
     override fun start(primaryStage: Stage) {
+        val root = BorderPane()
+        launch(CommonPool) {
+            delay(1000L)
+            println("hello")
+        }
+
+
         primaryStage.title = "Hello World"
-        primaryStage.scene = Scene(BorderPane(), 300.0, 275.0)
+        primaryStage.scene = Scene(root, 300.0, 275.0)
         primaryStage.show()
     }
 
---
+++ src/NetworkTable.kt
@@ -0,0 +1,12 @@
+import javafx.scene.control.TableView
+
+/**
+ * Created by qwerty on 1. 5. 2017.
+ */
+
+
+class NetworkNode( var address:String,var community:String)
+
+class NetworkTable:TableView<NetworkNode>() {
+
+}
