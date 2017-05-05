cbf22b617699cc24ea37296998e62788e3504087
qwerty2586 <qwerty2@students.zcu.cz>
2017-05-05 21:47:55 +0200

    nevim co stim je

--- src/NetworkTable.kt
+++ src/NetworkTable.kt
@@ -1,12 +1,40 @@
+import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.StringProperty
+import javafx.beans.value.ObservableValue
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
+import java.util.*
 
 /**
  * Created by qwerty on 1. 5. 2017.
  */
 
 
-class NetworkNode( var address:String,var community:String)
+class NetworkNode( _address:String,var _community:String) {
+    var address = SimpleStringProperty()
+        set(value) {
+            ip = value
+            dns = value
+        }
+    var ip = SimpleStringProperty()
+  //      set(value) {address = value}
+    var dns = SimpleStringProperty()
+  //      set(value) {address = value}
+    var community = SimpleStringProperty()
+    init {
+        address.value = _address
+        community.value = _community
+    }
+}
+
+
+val inserts = FXCollections.observableArrayList<NetworkNode>(arrayListOf(
+        NetworkNode("swkralovicka43","public")
+))
+
+
 
 class NetworkTable:TableView<NetworkNode>() {
 
@@ -14,7 +42,18 @@ class NetworkTable:TableView<NetworkNode>() {
     val ipColumn = TableColumn<NetworkNode,String>("IP")
 
     init {
+
+
+        dnsColumn.setCellValueFactory { v -> v.value.dns }
+        ipColumn.setCellValueFactory { v -> v.value.ip }
+
+        isEditable = true
+        ipColumn.isEditable = true
+        dnsColumn.isEditable = true
+
         columns.addAll(dnsColumn,ipColumn)
+
+        items.addAll(inserts)
     }
 
 }
